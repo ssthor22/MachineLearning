3f1cce73caccc63d3df081a70cc662e00888ad97
Seth <ssthor22@gmail.com>
2020-08-05 21:51:13 -0400

    Add files via upload
    LinRegGD.m - generalized to accept user-input datafile
    LogRegGD.m - generalized to accept user-input datafile, revisions to algorithm

--- GradientDescent/LogRegGD.m
+++ GradientDescent/LogRegGD.m
@@ -2,7 +2,7 @@
 #{
 Author:   Seth Thor
 Date:     08/03/2020
-Revised:  
+Revised:  08/05/2020
 Description:
   This script performs logistic regression gradient descent on a 
   given dataset. Sample data on gmat, gpa, work_experience, and admission
@@ -13,32 +13,89 @@ Description:
 
 clear;
 
+function data_norm = FeatureNormalization(data)
+  
+  % Normalize using mean and stddev
+  range_data = max(data) - min(data);
+  data_norm = data./range_data;
+  
+endfunction
+
+function [train_set, test_set] = SplitData(p, data)
+  
+  m = length(data);
+  nSample = floor(p*m); 
+  rndIDX = randperm(m); % random permutation of indices
+  
+  train_set = data(rndIDX(1:nSample), :);
+  test_set = data(rndIDX(nSample+1:m), :);  
+  
+endfunction
+
 function [J, h] = CostFunction(theta, X, y);
-  m = size(X, 2);                               % # of samples/columns
-  h = 1./( 1 .+ exp(-X*theta) );                % hypothesis function
-  J = ( 1/m ) * -y'*log(h) - (1-y)'*log(1-h);   % Cost function
+  
+  m = length(y);  % # of samples/columns
+  h = sigmoid(X*theta); % hypothesis function
+  J = ( 1/m ) * -y'*log(h) - (1-y)'*log(1-h); % Cost function
+
 endfunction
 
 function final_theta = GD(iter, alpha, theta, X, y)
-  m = size(X, 2);
+  
+  m = length(y);
   j = zeros(iter,1);
   for i = 1:iter
-    h = 1./( 1 .+ exp(-X*theta) );
+    h = sigmoid(X*theta);
     theta = theta - (alpha/m) * X'*(h - y); % Careful with dimensions here
     j(i) = CostFunction(theta, X, y);
   end
   final_theta = theta;
   plot([1:iter], j)
   xlabel('Iterations'); ylabel('Cost Function, J');
+  
+endfunction
+
+function g = sigmoid(z)
+
+% Computes the sigmoid of z, where z = X*theta
+  g = 1./( 1 .+ exp(-z) );
+  
+endfunction
+
+function accuracy = ModelPerformance(theta, X, y)
+  
+  z = X*theta;
+  y_predicted = sigmoid(z);
+
+  % Convert predictions into boolean (admit: 1, denied: 0)
+  % and calculate accuracy of prediction
+  y_pbool = zeros(length(y_predicted),1);
+  match = 0;
+  for i=1:length(y)
+    
+    if ( y_predicted(i) < 0.5 )
+      y_pbool(i) = 0;
+    elseif ( y_predicted(i) >= 0.5 )
+      y_pbool(i) = 1;
+    endif
+    
+  endfor
+    
+  accuracy = mean(double(y_pbool == y));
+
 endfunction
 
 #{
-Data must be in format:
+Data file must have data in format [x0 x1 x2 ... | y]
+After processing, the matrices will take the following dimensions:
+
  X: m(samples) x n(parameters)
  theta: n(parameters) x 1
- X*theta = h: 1 x m(samples)
+ theta*X = h: m(samples) x 1
  y: m(samples) x 1
+ 
 #}
+
 
 % Test dataset of admittance based on gmat, gpa, work experience
 % Taken from: https://datatofish.com/logistic-regression-python/
@@ -55,54 +112,36 @@ work_experience = [3;4;3;5;4;6;1;4;5;1;3;5;6;4;3;1;4;6;2;3;2;1;4;1;2;6;4;2;6;5;
                    1;2;4;6;5;1;2;1;4;5];
 admitted = [1;1;0;1;0;1;0;1;1;0;0;1;1;0;1;0;0;1;0;0;1;0;0;0;0;1;1;0;1;1;0;0;1;
             1;1;0;0;0;0;1];
+            
+data = [gmat, gpa, work_experience, admitted];
+
+%data = load('filename.txt');
+data_norm = FeatureNormalization(data);
+data_norm = [ones(length(data_norm),1), data_norm];
+
+m = length(data_norm); 
+n = size(data_norm, 2) - 1;
 
 % Use p% of dataset for training
 p = 0.25;
-
-% range of data to use for training
-train_len = length(gmat)*p; 
+[train_set, test_set] = SplitData(p, data_norm);
 
 %Design matrix - set to training data
-X = [ones(train_len, 1), gmat_norm(1:train_len), gpa(1:train_len), work_experience(1:train_len)];
-y = admitted(1:train_len);
+X = train_set(:,1:n);
+y = train_set(:,n+1);
 
 %Initialize theta
 theta = [0;0;0;0];
 
 % Perform gradient descent
 alpha = 0.01; % alpha, learning rate
-iter = 1000;  % iterations
+iter = 10000;  % iterations
 final_theta = GD(iter, alpha, theta, X, y)
 
 % Now use model to predict test set. Reset variables to test data
-s = length(gmat); % # of total samples
-X = [ones(s - train_len + 1, 1), gmat_norm(train_len:s), gpa(train_len:s), work_experience(train_len:s)];
-y = admitted(train_len:s);
-y_predicted = 1./(1 .+ exp(-X*final_theta));
+X = test_set(:,1:n);
+y = test_set(:,n+1);
 
-% Convert predictions into boolean (admit: 1, denied: 0)
-% and calculate accuracy of predictions
-y_pbool = zeros(length(y_predicted),1);
-match = 0;
-for i=1:length(y)
-  
-  if ( y_predicted(i) < 0.5 )
-    y_pbool(i) = 0;
-  elseif ( y_predicted(i) >= 0.5 )
-    y_pbool(i) = 1;
-  endif
-  
-  if ( y_pbool(i) == y(i) )
-    match = match + 1;
-  endif
-  
-end
-
-accuracy = match/length(y)
-figure
-plot([1:length(y_predicted)], y_pbool, 'x')
-hold on 
-plot([1:length(y_predicted)], y, 'o')
-hold off
+accuracy = ModelPerformance(final_theta, X, y)
 
 
